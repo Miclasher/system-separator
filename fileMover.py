605c59232168b412ef85a4dcbedc163a1acd5268
Miclasher <14122006mg@gmail.com>
2022-05-15 10:19:11 +0200

    Now u can create config names

--- fileMover.py
+++ fileMover.py
@@ -1,9 +1,14 @@
 import os
 import shutil
-USER_NAME = os.getlogin()
-dir = r"C:\Users\\{0}\Desktop\testconfig".format(USER_NAME)
-target = r"C:\Users\\{0}\Desktop".format(USER_NAME)
-tempArray = os.listdir(dir)
-tempArray.remove("wallpaper.jpg")
-for i in range(len(tempArray)):
-    shutil.move(dir + '\\' + tempArray[i], target)
+def moveFiles(configName="testconfig"):
+    USER_NAME = os.getlogin()
+    dir = r"C:\Users\\{0}\Desktop\\{1}".format(USER_NAME, configName)
+    target = r"C:\Users\\{0}\Desktop".format(USER_NAME)
+    tempArray = os.listdir(dir)
+    tempArray.remove("wallpaper.jpg")
+    for i in range(len(tempArray)):
+        shutil.move(dir + '\\' + tempArray[i], target)
+if __name__ == "__main__":
+    moveFiles()
+else:
+    pass
--- wallpaperSetter.py
+++ wallpaperSetter.py
@@ -1,3 +1,10 @@
 import ctypes
-dir = r'C:\Users\Miclasher\Desktop\testconfig\wallpaper.jpg'
-ctypes.windll.user32.SystemParametersInfoW(20, 0, dir, 0)
+from os import getlogin
+def setWallpaper(configName='testconfig'):
+    USER_NAME = getlogin()
+    dir = r'C:\Users\\{0}\Desktop\\{1}\wallpaper.jpg'.format(USER_NAME, configName)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, dir, 0)
+if __name__ == '__main__':
+    setWallpaper()
+else:
+    pass
